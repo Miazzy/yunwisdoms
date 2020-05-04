0eff7db7099250d45b58f2e8b80b29e0def441a2
miazzy <qq695926920@163.com>
2020-05-04 20:20:25 +0800

    init

--- request/request.js
+++ request/request.js
@@ -1,17 +1,31 @@
-//设置moment
-moment = null || moment;
-//设置superagent
-superagent = null || superagent;
-//定义工具
-window.tools = null || tools;
-//定义superagent
-window.superagent = null || superagent;
-//定义请求API
-window.requestAPI = null || requestAPI;
-//定义存储工具
-window.storage = null || storage;
-//定义流程API
-window.workflowAPI = null || workflowAPI;
+try {
+    //设置moment
+    moment = null || moment;
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    //设置superagent
+    superagent = null || superagent;
+} catch (error) {
+    console.error(error);   
+}
+
+try{
+    //定义工具
+    window.tools = null || tools;
+    //定义superagent
+    window.superagent = null || superagent;
+    //定义请求API
+    window.requestAPI = null || requestAPI;
+    //定义存储工具
+    window.storage = null || storage;
+    //定义流程API
+    window.workflowAPI = null || workflowAPI;   
+} catch(e) {
+    console.log(e)
+}
 
 try {
     /**
@@ -42,6 +56,7 @@ try {
     }
 
     window.queryQuestionById = queryQuestionById;
+
 } catch (error) {
     console.log(error);
 }
