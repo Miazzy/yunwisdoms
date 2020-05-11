dc8382037d3f3131320370d06d9c4247f8d05494
miazzy <qq695926920@163.com>
2020-05-11 21:18:16 +0800

    init

--- request/topics.js
+++ request/topics.js
@@ -1,31 +1,37 @@
-const topicPostURL = `https://api.shengtai.club/topics/{topic}/messages?key=default&sync`;
-const topicGetURL = `https://api.shengtai.club/topics/{topic}/messages?group=0`;
+const topicPostURL = `https://kafka.shengtai.club/topics/{topic}/messages?key=default&sync`;
+const topicGetURL = `https://kafka.shengtai.club/topics/{topic}/messages?group=0`;
 
 /**
  * @function 通过Kafka推送消息函数
  * @param {*} topic 发布/订阅消息的topic
  * @param {*} message 推送的消息
  */
-async function sendTopic(topic, message) {
+async function sendTopic(topic, message, sender) {
 
-    var content = {
-        sender: topic,
-        message: message,
-        timestamp: new Date().getTime()
+    try {
+        var content = {
+            receiver: topic,
+            message: message,
+            sender: sender,
+            timestamp: new Date().getTime()
+        }
+
+        message = await superagent
+            .post(topicPostURL.replace('{topic}', topic))
+            .send(JSON.stringify(content))
+            .set('Content-Type', 'text/plain')
+            .set('accept', 'text')
+            .end((err, res) => {
+                console.log(err, res);
+            });
+
+        console.log(` Kafka推送消息：`, content, ` 返回结果：`, message);
+
+        return message;
+    } catch (error) {
+        console.log(error);
     }
 
-    message = await superagent
-        .post(topicPostURL.replace('{topic}', topic))
-        .send(JSON.stringify(content))
-        .set('Content-Type', 'text/plain')
-        .set('accept', 'text')
-        .end((err, res) => {
-            console.log(err, res);
-        });
-
-    console.log(` Kafka推送消息：`, content, ` 返回结果：`, message);
-
-    return message;
 }
 
 window.sendTopic = sendTopic;
@@ -40,16 +46,21 @@ window.sendTopic = sendTopic;
  */
 async function queryTopic(topic, group, noask, offset, partionID) {
 
-    var url = topicGetURL.replace('{topic}', topic);
-    var message = await superagent.get(url);
+    try {
+        var url = topicGetURL.replace('{topic}', topic);
+        var message = await superagent.get(url);
 
-    message = message.body;
-    message.key = decodeBase64(message.key);
-    message.value = decodeBase64(message.value);
+        message = message.body;
+        message.key = decodeBase64(message.key);
+        message.value = decodeBase64(message.value);
 
-    console.log(` Kafka订阅消息：`, topic, ` 返回结果：`, message);
+        console.log(` Kafka订阅消息：`, topic, ` 返回结果：`, message);
 
-    return message;
+        return message;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 window.queryTopic = queryTopic;
@@ -59,7 +70,11 @@ window.queryTopic = queryTopic;
  * @param {*} message 
  */
 function decodeBase64(message) {
-    return decodeURIComponent(window.atob(message));
+    try {
+        return decodeURIComponent(window.atob(message));
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 window.decodeBase64 = decodeBase64;
