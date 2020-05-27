a0dbaa05b16afac1a498f17848ec61b5d92033cc
miazzy <qq695926920@163.com>
2020-05-27 11:29:32 +0800

    init

---
+++ request/keepssl.js
@@ -0,0 +1,9 @@
+//http协议
+let protocol = window.location.host.includes('localhost') ? 'http:' : 'https:';
+//刷新切换
+if (window.location.href !== protocol + '//' + window.location.host + '/') {
+    //缓存原URL地址
+    window.sessionStorage.setItem('system_root_url', window.location.href.replace(window.location.origin, ''));
+    //跳转到URL根地址
+    window.location.href = protocol + '//' + window.location.host;
+}
