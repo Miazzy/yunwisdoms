ac7d9159f51f0c800e949485b15d1bfe63163cd5
miazzy <qq695926920@163.com>
2020-06-02 17:10:05 +0800

    init

--- request/keeptokenvld.js
+++ request/keeptokenvld.js
@@ -31,7 +31,7 @@ const yunpanToken = async(that, UUID, md5) => {
         response = localStorage.getItem(`system_user_stoken:${stoken}`);
         let tflag = (response == null || typeof response == 'undefined' || response == '');
         if (tflag) {
-            var url = `https://www.shengtai.club/jeecg-boot/sys/user/list?pageSize=0`;
+            let url = `https://www.shengtai.club/jeecg-boot/sys/user/list?pageSize=0`;
             response = (await superagent.get(url).set('x-access-token', stoken))['body'];
             localStorage.setItem(`system_user_stoken:${stoken}`, JSON.stringify(response));
         } else {
@@ -96,7 +96,7 @@ const yunpanToken = async(that, UUID, md5) => {
     //如果未获取到用户缓存信息，查询用户信息
     if (pflag || userinfo.username !== that.username) {
         try {
-            var url = `https://api.shengtai.club/api/tank30_user?_where=(username,eq,${that.username})`;
+            let url = `https://api.shengtai.club/api/tank30_user?_where=(username,eq,${that.username})`;
             response = (await superagent.get(url))['body'];
             console.log(response);
             localStorage.setItem(`system_pan_vue_user_info`, JSON.stringify(response[0]));
@@ -118,3 +118,88 @@ const yunpanToken = async(that, UUID, md5) => {
 }
 
 window.yunpanToken = yunpanToken;
+
+const webchatToken = async(that, UUID, md5) => {
+
+    //清空缓存localstorage
+    window.localStorage.removeItem(`vue-device-id`);
+    window.localStorage.removeItem(`vue-token`);
+    window.localStorage.removeItem(`vue-user-id`);
+
+    var response = null;
+
+    //获取username and token
+    var username = that.querystring('username');
+    var token = that.querystring('token');
+    var stoken = that.querystring('token');
+    var hashtoken = that.querystring('hashtoken');
+    var hashkey = that.querystring('hashkey');
+    var sfingerprint = that.querystring('fingerprint');
+    var date = dayjs().format('YYYY-MM-DD');
+    var tokenname = '';
+
+    //计算有效md5
+    var validmd5 = md5(`u:${username}|t:${token}|s:${stoken}|h:${hashtoken}|k:${hashkey}|f:${sfingerprint}|d:${date}`);
+
+    //获取缓存中的md5value
+    var validmd5value = localStorage.getItem(`system_valid_md5_value`);
+
+    //计算指纹
+    var cfingerprint = await that.calcuFingerPrint();
+
+    //检查stoken、hashtoken、hashkey、fingerprint是否合法
+
+    //指纹认证失败
+    if (sfingerprint !== cfingerprint || sfingerprint.length != 128) {
+        that.$message.error('AccessToken认证失败！[0x01指纹认证失败]')
+        return;
+    }
+
+    //已经验证过，自动登录
+    if (validmd5 === validmd5value) {
+        that.autologin(username);
+        return;
+    }
+
+    //服务端stoken认证
+    try {
+        let url = `https://www.shengtai.club/jeecg-boot/sys/user/list?pageSize=0`;
+        response = (await superagent.get(url).set('x-access-token', stoken))['body'];
+        console.log(response);
+    } catch (error) {
+        response = null;
+        console.log(error);
+    }
+
+    if (response == null || typeof response == 'undefined' || response.status == 500 || !(response.code == 0 && response.success == true)) {
+        that.$message.error('AccessToken认证失败！[0x01服务端Token认证失败]')
+        return;
+    }
+
+    //客户端认证stoken和username是否匹配
+    try {
+        tokenname = JSON.parse(window.atob(stoken.split('.')[1]));
+    } catch (error) {
+        console.log(error);
+    }
+
+    if (username !== tokenname.username) {
+        that.$message.error('AccessToken认证失败！[0x02TokenName认证失败]')
+        return;
+    }
+
+    //校验token是否合法
+    if (!(md5(hashkey + date + username) === hashtoken && hashtoken.startsWith('0000') && hashkey.length == 128)) {
+        that.$message.error("AccessToken认证失败！[0x03Token异常权限校验失败]");
+        return;
+    }
+
+    //验证通过深圳validmd5
+    localStorage.setItem(`system_valid_md5_value`, validmd5);
+
+    //执行登录操作
+    that.autologin(username);
+
+}
+
+window.webchatToken = webchatToken;
