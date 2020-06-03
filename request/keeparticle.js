a03e57ccb9d53aa764656dc37560ef41a64d0124
miazzy <qq695926920@163.com>
2020-06-03 11:32:44 +0800

    init

--- request/keeparticle.js
+++ request/keeparticle.js
@@ -1,4 +1,9 @@
 const articleLoadData = async(that, storage, manageAPI) => {
+    //将that挂载到blogArticle上面
+    if (window.blogArticle == null || typeof window.blogArticle == 'undefined') {
+        window.blogArticle = that;
+    }
+    //获取当前key信息
     var key = storage.getStore(`system_title_key`);
     //博文数据
     var blist = await manageAPI.queryBlogInfo(that.page, that.size, key);
@@ -15,3 +20,27 @@ const articleLoadData = async(that, storage, manageAPI) => {
 }
 
 window.articleLoadData = articleLoadData;
+
+const articleLoadMore = async(that, storage, manageAPI) => {
+    //将that挂载到blogArticle上面
+    if (window.blogArticle == null || typeof window.blogArticle == 'undefined') {
+        window.blogArticle = that;
+    }
+    //获取当前key信息
+    var key = storage.getStore(`system_title_key`);
+    //显示加载图标
+    that.loadingMore = true;
+    //获取返回数据结果
+    var blist = await manageAPI.queryBlogInfo(that.page, that.size, key);
+    //数据合并
+    that.data = that.data.concat(blist);
+    //新增查询页面
+    that.page++;
+    //延时加载数据
+    setTimeout(async() => {
+        //关闭加载图标
+        that.loadingMore = false;
+    }, 1000);
+}
+
+window.articleLoadMore = articleLoadMore;
