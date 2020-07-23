713c130b6696ede6db3e0e7523d7c4e86aafcf1d
miazzy <qq695926920@163.com>
2020-07-23 18:29:06 +0800

    init

---
+++ js/servicework.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', function(event) {
+    console.log(event);
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/index.html',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response !== undefined) {
+            return response;
+        } else {
+            return fetch(event.request).then(function(response) {
+                let responseClone = response.clone();
+                if (/\.(png|jpg|gif|html|js|css|ico|svg|jpeg|doc|docx|ppt|pptx|xls|xlsx|pdf|eot|ttf|woff|woff2|json|txt)$/.test(event.request.url.toLocaleLowerCase())) {
+                    caches.open('v1').then(function(cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                }
+                return response;
+            }).catch(function() {
+                return false;
+            });
+        }
+    }));
+});
